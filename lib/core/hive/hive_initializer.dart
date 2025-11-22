9a71d95fcfe42b3c316ed5c6ab202f1a9ad1095b
daffle05 <torlaodafny@gmail.com>
2025-11-23 07:19:43 +0800

    Fix hive_initializer.dart

--- lib/core/hive/hive_initializer.dart
+++ lib/core/hive/hive_initializer.dart
@@ -1,18 +1,19 @@
 import 'package:hive/hive.dart';
+import 'package:rentmate/features/data/dashboards/dashboard_model.dart';
 import 'package:rentmate/features/data/models/tenant_model.dart';
-import 'package:rentmate/features/tenant/data/models/payment_model.dart'; // Ensure this is the correct path or define the adapter
+// Corrected import path
+import 'package:rentmate/features/tenant/data/models/payment_model.dart'; // Import generated adapter
 
 Future<void> registerHiveAdapters() async {
   if (!Hive.isAdapterRegistered(0)) {
     Hive.registerAdapter(TenantModelAdapter());
   }
+
   if (!Hive.isAdapterRegistered(1)) {
     Hive.registerAdapter(PaymentModelAdapter());
   }
-  if (!Hive.isAdapterRegistered(3)) {
-    Hive.registerAdapter(DashboardSummaryModelAdapter() as TypeAdapter); // Ensure DashboardSummaryModelAdapter is correctly imported and defined
-  }
-}
 
-class DashboardSummaryModelAdapter {
+  if (!Hive.isAdapterRegistered(3)) {
+    Hive.registerAdapter(DashboardSummaryModelAdapter());
+  }
 }
